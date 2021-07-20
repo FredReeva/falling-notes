133380c310bd3dfd0a25c5090b6a3f2c72d3abc9
Federico Maver <maver.federico@gmail.com>
2021-07-20 22:45:06 +0200

    style menu estetica

--- src/components/menu_styles/StyleMenu.js
+++ src/components/menu_styles/StyleMenu.js
@@ -9,22 +9,31 @@ const StyledColorPicker = styled(HuePicker)`
     display: flex;
     flex-direction: row;
     margin-top: 10px;
-
     align-self: center;
     cursor: pointer;
+`;
+
+const StyledPointer = styled.div`
+    width: 40px;
+    height: 40px;
+    transform: translate(-50%, -5px);
+    border-radius: 50%;
+    background: ${(props) => props.theme.buttonColor};
 `;
 
 const StyleMenu = (props) => {
     const toggleMenu = () => {
         props.toggleMenu(2);
     };
-
+    //console.log(props.color);
     return props.showMenu ? (
         <div className="Container">
             <BlurredPage onClick={toggleMenu} />
             <ModalMenu className="StyleMenu">
                 <HeaderMenu titleMenu={'Style Lab'} toggleMenu={toggleMenu} />
                 <StyledColorPicker
+                    height={30}
+                    pointer={StyledPointer}
                     color={props.color}
                     onChangeComplete={(color) => {
                         props.setColor(color);
