52651f67e68a24357a44757180a80a12fb0ee9fc
Federico Maver <maver.federico@gmail.com>
2021-07-20 11:20:44 +0200

    pause problem almost fixed

--- src/components/MidiCreator.js
+++ src/components/MidiCreator.js
@@ -1,4 +1,6 @@
 import { IoPause } from 'react-icons/io5';
+import { convert, chordNotes } from '../libraries/melodygen/utils.js';
+import { MidiWriter } from 'midi-writer-js';
 
 export const createMidi = (melody, chords) => {
     var MidiWriter = require('midi-writer-js');
@@ -8,18 +10,39 @@ export const createMidi = (melody, chords) => {
 
     var pause = 0;
 
+    const convertDurationValues = (dur) => {
+        //console.log('converting' + dur);
+        if (dur === 3) return 'd2';
+        if (dur === 5) return ['d2', '2'];
+        if (dur === 6) return ['d2', 'd2'];
+        if (dur === 7) return ['d2', '1'];
+        if (dur === 9) return ['d2', '2', '1'];
+        if (dur === 10) return ['d2', 'd2', '1'];
+        if (dur === 11) return ['d2', '1', '1'];
+        if (dur === 12) return ['1', '1', '1'];
+        //! finish
+        if (dur === 13) return '';
+        if (dur === 14) return '';
+        if (dur === 15) return '';
+
+        return dur;
+    };
+
     melody.forEach((note) => {
         if (note['type'] === 'pause') {
             pause = pause + 1 / (note['duration'] * 2);
         }
 
         if (note['type'] === 'note') {
+            pause = convertDurationValues(pause);
+            //console.log(pause);
+
             melodyMidi.addEvent(
                 [
                     new MidiWriter.NoteEvent({
                         pitch: note['pitch'],
                         duration: 1 / (note['duration'] * 2),
-                        wait: pause === 6 ? 4 : pause,
+                        wait: pause,
                     }),
                 ],
 
@@ -32,10 +55,29 @@ export const createMidi = (melody, chords) => {
         }
     });
 
-    // chords.forEach((chord) => {
-    //     console.log(chord['tonic'])
+    // var notesInChords = [];
+    // var convertChord = { tonic: 'C', color: 'Major' };
 
+    // chords.forEach((chord) => {
+    //     convertChord.tonic = chord['tonic'];
+    //     convertChord.color = chord['quality'];
+    //     notesInChords.push(chordNotes(convertChord, 2));
+
+    //     melodyMidi.addEvent(
+    //         [
+    //             new MidiWriter.NoteEvent({
+    //                 pitch: note['pitch'],
+    //                 duration: 1 / (chord['duration'] * 2),
+    //                 wait: pause,
+    //             }),
+    //         ],
+
+    //         function (event, index) {
+    //             return { sequential: true };
+    //         }
+    //     );
     // });
+    // console.log(notesInChords);
 
     //console.log(melodyMidi);
 
