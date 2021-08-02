c9983422f2c3cac42bb40b778d464be31b499992
Federico Maver <maver.federico@gmail.com>
2021-08-02 10:27:42 +0200

    added link to repository in help

--- src/components/help page/HelpMenu.js
+++ src/components/help page/HelpMenu.js
@@ -30,17 +30,49 @@ const HelpMenu = (props) => {
                 </h1>
                 <p>&nbsp;&nbsp;</p>
                 <h4>INSTRUCTIONS:</h4>
-                <p>1. Open the "Song Selection" menu to create a song or to select it from the existing list </p>
-                <p>2. Open the "Chord Lab" menu to write the chord progression of your song</p>
-                <p>3. Click on the dice to generate a random melody. Every time you press this button, the old melody will be overwritten!</p>
+                <p>
+                    1. Open the "Song Selection" menu to create a song or to
+                    select it from the existing list{' '}
+                </p>
+                <p>
+                    2. Open the "Chord Lab" menu to write the chord progression
+                    of your song
+                </p>
+                <p>
+                    3. Click on the dice to generate a random melody. Every time
+                    you press this button, the old melody will be overwritten!
+                </p>
                 <p>4. Press the Play button and enjoy your creation! </p>
                 <p>&nbsp;&nbsp;</p>
                 <h4>CUSTOMIZATION:</h4>
-                <p>The "Style Lab" menu allows you to choose the color accent and the instruments of your song</p>
-                <p>The "Parameters Lab" menu allows you to choose the tempo of the song and tune the melody generator parameters</p>
+                <p>
+                    The "Style Lab" menu allows you to choose the color accent
+                    and the instruments of your song
+                </p>
+                <p>
+                    The "Parameters Lab" menu allows you to choose the tempo of
+                    the song and tune the melody generator parameters
+                </p>
                 <p>&nbsp;&nbsp;</p>
                 <h4>REMEMBER:</h4>
-                <p>If you really like the melody, you can download the MIDI file using the Download button!</p>
+                <p>
+                    If you really like the melody, you can download the MIDI
+                    file using the Download button!
+                </p>
+                <p>&nbsp;&nbsp;</p>
+                <h4>EXTRA:</h4>
+                <p>
+                    For more infos, please take a look at the&nbsp;
+                    <a
+                        style={{ color: '#ffbf00' }}
+                        href="https://github.com/FredReeva/falling-notes"
+                        target="_blank"
+                        rel="noreferrer"
+                    >
+                        Github Repository
+                    </a>
+                    .
+                </p>
                 <p>&nbsp;&nbsp;</p>
                 <p className="pulsate">Click anywhere to continue...</p>
             </StyledHelperSection>
